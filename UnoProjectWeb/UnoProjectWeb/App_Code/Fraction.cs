9eebc6c4c7c24744a6c2fd09b0e0e50641bb96ac
chill-out <denis.adamcho@gmail.com>
2015-04-05 19:08:55 +0300

    added Fraction.cs
    added Fraction.cs

---
+++ UnoProjectWeb/UnoProjectWeb/App_Code/Fraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnoProjectWeb.App_Code
+{
+    public class Fraction
+    {
+        public bool endGame;
+        public int opponentCards;
+        public ArrayList Cards;
+        public Card lastCard;
+    }
+}
