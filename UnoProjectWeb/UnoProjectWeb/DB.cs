3b38dbcc13aeff7f5e7923d00304fdfce71ccfbc
AviranJ <aviranj@gmail.com>
2015-04-17 22:48:17 +0300

    Fix DB syntax

--- UnoProjectWeb/UnoProjectWeb/DB.cs
+++ UnoProjectWeb/UnoProjectWeb/DB.cs
@@ -18,33 +18,29 @@ namespace UnoProjectWeb
     {
         private static string _ConStr = WebConfigurationManager.ConnectionStrings["AConnectionString"].ConnectionString;
 
-        public static HttpContext CurrentContext
+        public static DataTable ExecuteSelect(string tableName, string sqlSelect, params SqlParameter[] paramsList)
         {
-            get
-            {
-                return HttpContext.Current;
-            }
-        }
+            
+             SqlConnection myConnect;
+             SqlCommand myCommand;
+             SqlDataReader myDataReader;
+             myConnect = new SqlConnection(_ConStr);
+             myConnect.Open();
 
-        public static SqlDataAdapter CreateDataAdapter(string strSql, IDbConnection conn)
-        {
-            return new SqlDataAdapter(strSql, (SqlConnection)conn);
-        }
+             myCommand = new SqlCommand(sqlSelect, myConnect);
 
-        public static DataTable ExecuteSelect(string sqlSelect, SqlParameter[] paramsList, string tableName)
-        {
             DataTable dataTable = new DataTable();
             dataTable.TableName = tableName;
-            using (SqlConnection conn = new SqlConnection(ConStr))
+
+            using (SqlConnection conn = new SqlConnection(_ConStr))
             {
-                SqlDataAdapter adapter = CreateDataAdapter(sqlSelect, conn);
                 if (paramsList != null)
                 {
                     foreach (SqlParameter parameter in paramsList)
                     {
                         if (parameter != null)
                         {
-                            adapter.SelectCommand.Parameters.Add(parameter);
+                            myCommand.Parameters.Add(parameter);
                         }
                     }
                 }
@@ -52,16 +48,15 @@ namespace UnoProjectWeb
                 try
                 {
                     conn.Open();
-                    adapter.Fill(dataTable);
+                    myDataReader = myCommand.ExecuteReader();
+                    dataTable.Load(myDataReader);
+
                 }
                 catch (Exception exception)
                 {
                 }
                 finally
                 {
-                    adapter.Dispose();
-                    adapter = null;
-
                     conn.Close();
                 }
             }
@@ -69,70 +64,12 @@ namespace UnoProjectWeb
             return dataTable;
         }
 
-        public static DataTable ExecuteSelect(string tableName, string sqlSelect, params SqlParameter[] paramsList)
-        {
-            return ExecuteSelect(sqlSelect, paramsList, tableName);
-        }
-
-        public static DataTable ExecuteSelect(string sqlSelect, SqlParameter param, string tableName)
-        {
-            return ExecuteSelect(sqlSelect, new SqlParameter[] { param }, tableName);
-        }
-
-        public static DataTable ExecuteSelect(string sqlSelect)
-        {
-            return ExecuteSelect(sqlSelect, new SqlParameter[] { }, "TABLE_SELECT");
-        }
-
-        public static string SelectField(string fieldName, string tableName, string whereClause, string orderByClause)
-        {
-            string tempField = "";
-
-            string strSql = "SELECT " + fieldName + " FROM " + tableName + " ";
-            if (whereClause != null && whereClause != "")
-            {
-                strSql += "WHERE " + whereClause + " ";
-            }
-            if (orderByClause != null && orderByClause != "")
-            {
-                strSql += "ORDER BY " + orderByClause + " ";
-            }
-
-            try
-            {
-                DataTable tempDT = ExecuteSelect(strSql, new SqlParameter[] { }, tableName);
-
-                tempField = tempDT.Rows[0][0].ToString().Trim();
-            }
-            catch (Exception exception)
-            {
-                throw new Exception(strSql, exception);
-            }
-
-            return tempField;
-        }
-
-
-        public static int ExecuteNonQuery(string cmdText)
-        {
-            return ExecuteNonQuery(cmdText, new SqlParameter[] { });
-        }
-
         public static int ExecuteNonQuery(string cmdText, params SqlParameter[] paramsList)
         {
-            return ExecuteNonQuery(cmdText, null, paramsList);
-        }
-
-        public static int ExecuteNonQuery(string cmdText, string conStr, params SqlParameter[] paramsList)
-        {
-            if (String.IsNullOrEmpty(conStr))
-            {
-                conStr = ConStr;
-            }
 
             int num = 0;
 
-            using (SqlConnection conn = new SqlConnection(conStr))
+            using (SqlConnection conn = new SqlConnection(_ConStr))
             {
 
                 SqlCommand command = new SqlCommand(cmdText, conn);
@@ -143,8 +80,6 @@ namespace UnoProjectWeb
                 }
                 try
                 {
-                    //DB.WriteTrace("Conns: " + FbConnection.GetPooledConnectionCount(conn).ToString() + "  Pools: " + FbConnection.ConnectionPoolsCount.ToString());
-
                     command.Connection.Open();
                     num = command.ExecuteNonQuery();
                 }
@@ -160,53 +95,6 @@ namespace UnoProjectWeb
             }
 
             return num;
-        }
-
-        public static int ExecuteNonQueryWithoutException(string cmdText, params SqlParameter[] paramsList)
-        {
-            int num = 0;
-
-            using (SqlConnection conn = new SqlConnection(ConStr))
-            {
-
-                SqlCommand command = new SqlCommand(cmdText, conn);
-                foreach (SqlParameter parameter in paramsList)
-                {
-                    command.Parameters.Add(parameter);
-                }
-                try
-                {
-                    command.Connection.Open();
-                    num = command.ExecuteNonQuery();
-                }
-                catch (Exception exception)
-                {
-                    num = -1;
-                }
-                finally
-                {
-                    command.Dispose();
-                    command = null;
-                    conn.Close();
-                }
-            }
-
-            return num;
-        }
-
-
-        public static string ConStr
-        {
-            get
-            {
-                if (_ConStr == null)
-                {
-
-                    _ConStr = WebConfigurationManager.ConnectionStrings["AConnectionString"].ConnectionString;
-                }
-
-                return _ConStr;
-            }
         }
 
     }
